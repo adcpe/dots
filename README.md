1eb4123958ed5f24181ff40d05c99f90b1fabf89
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-04 02:23:43 -0500

    Added README and restore script

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dotfiles
+
+## Requirements
+
+- Git
+- Curl
+
+## Restore script
+
+Install config tracking in your \$HOME by running:
+
+```
+curl -Lks https://raw.githubusercontent.com/echounit1/dots/master/restore.sh | /bin/bash
+```
+
+## Git commands
+
+`dots` acts as an alias for `git` in the `$HOME` directory.
+
+---
+
+_As seen in https://www.atlassian.com/git/tutorials/dotfiles_
---
+++ restore.sh
@@ -0,0 +1,14 @@
+git clone --bare https://github.com/echounit1/dots.git $HOME/dots.git
+function dots {
+  /usr/bin/git --git-dir=$HOME/dots.git/ --work-tree=$HOME $@
+}
+mkdir -p dots-backup
+dots checkout
+if [ $? = 0 ]; then
+  echo "Checked out dots.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} dots-backup/{}
+fi;
+dots checkout
+dots config status.showUntrackedFiles no
