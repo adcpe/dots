d15211f049ed118fd3dad129b8817e5f853aafec
Andrés Del Carpio <andresdc@andresdc.net>
2020-04-14 20:31:02 -0500

    Added Oh My ZSH and plugins to the restore script

--- restore.sh
+++ restore.sh
@@ -1,15 +1,25 @@
 git clone --bare https://github.com/echounit1/dots.git $HOME/.dots-git
+
 function dots {
   /usr/bin/git --git-dir=$HOME/.dots-git/ --work-tree=$HOME $@
 }
+
 mkdir -p .dots-backup
 dots checkout
+
 if [ $? = 0 ]; then
   echo "Checked out dots.";
   else
     echo "Backing up pre-existing dot files.";
     dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dots-backup/{}
 fi;
+
 dots checkout
 dots config status.showUntrackedFiles no
 dots remote set-url origin git@github.com:echounit1/dots.git
+
+# configure oh my zsh and plugins
+git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
