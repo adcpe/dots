42b6adfed758a0fb8fb9d72b2e5dbfc23cc016b7
Andrés Del Carpio <andresdc@andresdc.net>
2021-03-25 18:35:57 -0500

    Removed vim keybindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,7 @@
-fish_vi_key_bindings
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
-
 set EDITOR nano
 set VISUAL nano
 set XDG_CONFIG_HOME ~/.config
 
-source ~/.config/fish/alias.fish
 source ~/.asdf/asdf.fish
+source ~/.config/fish/alias.fish
 starship init fish | source
