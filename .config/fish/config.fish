8b6b7051f55ed4696548befbb7cd8f055f5e79b0
Andrés Del Carpio <andresdc@andresdc.net>
2022-11-18 10:51:22 -0500

    Added pnpm the config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ set EDITOR nano
 set VISUAL nano
 set XDG_CONFIG_HOME ~/.config
 
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+
 source ~/.asdf/asdf.fish
 source ~/.config/fish/alias.fish
 starship init fish | source
