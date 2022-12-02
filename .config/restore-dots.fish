8f3512d6e1eccb2413bf1d91760700e84029df57
Andrés Del Carpio <andresdc@andresdc.net>
2022-12-02 14:42:39 -0500

    Enh: re-do restore script
    - Moved into .config folder
    - Added prompt question
    - Re-written in fish

---
+++ .config/restore-dots.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/env fish
+echo -e 'This operation will overwrite the current settings in $HOME directory.'
+read -l -P 'Do you want to continue? [y/N] ' confirm
+
+switch $confirm
+    case Y y
+        function conf
+            /usr/bin/git --git-dir=$dir --work-tree=$HOME $argv
+        end
+
+        echo -e 'Cloning https://gitlab.com/adcpe/dots.git into .dots-git'
+        set -l dir $HOME/.dots-git/ # set dots git directory
+
+        rm -R -f $dir
+        git clone --bare https://gitlab.com/adcpe/dots.git $dir
+
+        echo -e 'Cloning https://gitlab.com/adcpe/dots.git into .dots-git'
+        conf checkout -f # checkout overwriting existing files
+        conf config status.showUntrackedFiles no # ignore untracked files
+        conf remote set-url origin git@gitlab.com:adcpe/dots.git # use ssh for remote
+        echo -e 'Operation finished!'
+    case '*'
+        echo -e 'Operation canceled.'
+end
