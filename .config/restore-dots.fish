fa680f246b4d5edb4166dc922091520ec74c6f3f
Andrés Del Carpio <andresdc@andresdc.net>
2022-12-02 15:22:33 -0500

    Update restore script

--- .config/restore-dots.fish
+++ .config/restore-dots.fish
@@ -5,20 +5,16 @@ read -l -P 'Do you want to continue? [y/N] ' confirm
 switch $confirm
     case Y y
         set -l dir $HOME/.dots-git/ # set dots git directory
-
-        function conf
-            /usr/bin/git --git-dir=$dir --work-tree=$HOME $argv
-        end
+        rm -R -f $dir
 
         echo -e 'Cloning https://gitlab.com/adcpe/dots.git into .dots-git'
 
-        rm -R -f $dir
         git clone --bare https://gitlab.com/adcpe/dots.git $dir
 
         echo -e 'Cloning https://gitlab.com/adcpe/dots.git into .dots-git'
-        conf checkout -f # checkout overwriting existing files
-        conf config status.showUntrackedFiles no # ignore untracked files
-        conf remote set-url origin git@gitlab.com:adcpe/dots.git # use ssh for remote
+        /usr/bin/git --git-dir=$dir --work-tree=$HOME checkout -f # checkout overwriting existing files
+        /usr/bin/git --git-dir=$dir --work-tree=$HOME config status.showUntrackedFiles no # ignore untracked files
+        /usr/bin/git --git-dir=$dir --work-tree=$HOME remote set-url origin git@gitlab.com:adcpe/dots.git # use ssh for remote
         echo -e 'Operation finished!'
     case '*'
         echo -e 'Operation canceled.'
