2a52b959809c390ab0ec3855dc0f38da49a3f310
Andrés Del Carpio <andresdc@andresdc.net>
2022-12-04 00:09:47 -0500

    Added more abbreviations

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,14 +1,13 @@
 # pacman/yay
 abbr -a paci 'yay -S'
-abbr -a pacun 'yay -Rns'
+abbr -a pacun 'yay -Rcs'
 abbr -a pacu 'yay -Syu'
 abbr -a pacup 'yay -Syyu'
-abbr -a pacro 'yay -Rns (yay -Qdtq)'
+abbr -a pacro 'yay -Rcs (yay -Qdtq)'
 abbr -a pacs 'yay -Ss'
 abbr -a pacsl 'yay -Qs'
 abbr -a paccc 'yay -Sc'
-abbr -a paclo 'yay -Qdt'
-abbr -a pacinfo 'yay -Si'
+abbr -a pacsi 'yay -Si'
 
 # dotfiles repo at $HOME
 abbr -a dadd 'dots add'
@@ -19,12 +18,43 @@ abbr -a dcom 'dots commit'
 
 # pnpm
 abbr -a pni 'pnpm i'
+abbr -a pnun 'pnpm un'
 abbr -a pnid 'pnpm i -D'
 abbr -a pnig 'pnpm i -g'
 abbr -a pnx 'pnpm dlx'
 
+# npm
+abbr -a npi 'pnpm i'
+abbr -a npun 'pnpm un'
+abbr -a npid 'pnpm i -D'
+abbr -a npig 'pnpm i -g'
+
+# protonvpn
+abbr -a protin 'protonvpn-cli login'
+abbr -a protc 'protonvpn-cli connect'
+abbr -a protcf 'protonvpn-cli connect -f'
+abbr -a protdc 'protonvpn-cli disconnect'
+abbr -a protst 'protonvpn-cli status'
+
+# asdf
+abbr -a afpad 'asdf plugin add'
+abbr -a afprm 'asdf plugin remove'
+abbr -a afpls 'asdf plugin list'
+abbr -a afpup 'asdf plugin update --all'
+abbr -a afi 'asdf install'
+abbr -a afun 'asdf uninstall'
+abbr -a afls 'asdf list'
+abbr -a aflsa 'asdf list all'
+abbr -a afcurr 'asdf current'
+
+# exa/ls
+abbr -a ls 'exa --group-directories-first --icons'
+abbr -a lsa 'exa --group-directories-first --icons --all'
+abbr -a lsg 'exa --group-directories-first --icons --git'
+abbr -a lst 'exa --group-directories-first --icons --tree'
+
 # various
-abbr -a cat 'bat'
-abbr -a ls 'exa --group-directories-first'
-abbr -a rm 'trash'
+abbr -a cat bat
+abbr -a man batman
+abbr -a rm trash
 abbr -a ssh 'kitty +kitten ssh'
