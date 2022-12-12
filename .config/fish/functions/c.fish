e5519e74a25d5948fb135cbc1ee0e743ae2ec71b
Andrés Del Carpio <adc@adc.pe>
2022-12-12 05:39:15 -0500

    Update functions

--- .config/fish/functions/c.fish
+++ .config/fish/functions/c.fish
@@ -1,4 +1,4 @@
-function c -d 'Opens VSCode/VSCodium on the current directory'
+function c -w codium -d 'Opens VSCode/VSCodium on the current directory'
     if command --search --quiet code
         code $argv
     else
--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,3 +1,7 @@
-function dots -d='Shortcut for git operations my dotfiles repository'
-    git --git-dir=$HOME/.dots-git/ --work-tree=$HOME $argv
+function dots -d='Shortcut for git operations on my dotfiles repository'
+    if test (command pwd) = ~
+        git --git-dir=$HOME/.config/dots/git/ --work-tree=$HOME $argv
+    else
+        echo -e "You can't do that here."
+    end
 end
