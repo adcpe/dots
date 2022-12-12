82704e89b7f84f341770970bd56e8793baacfd02
Andrés Del Carpio <adc@adc.pe>
2022-12-12 05:50:16 -0500

    Amend

--- .config/fish/functions/dots.fish
+++ .config/fish/functions/dots.fish
@@ -1,6 +1,6 @@
 function dots -d='Shortcut for git operations on my dotfiles repository'
     if test (command pwd) = ~
-        git --git-dir=$HOME/.config/dots/git/ --work-tree=$HOME $argv
+        git --git-dir=$HOME/.config/dots/git --work-tree=$HOME $argv
     else
         echo -e "You can't do that here."
     end
