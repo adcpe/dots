40a0b841282cc8f01e62dfa59f0adcd34613a967
Andrés Del Carpio <adc@adc.pe>
2022-12-12 07:42:39 -0500

    Add hook

--- .config/fish/functions/c.fish
+++ .config/fish/functions/c.fish
@@ -1,7 +1,7 @@
-function c -w codium -d 'Opens VSCode/VSCodium on the current directory'
-    if command --search --quiet code
-        code $argv
-    else
+function c -w codium -d 'Opens VSCodium/VSCode on the current directory'
+    if command --search --quiet codium
         codium $argv
+    else
+        code $argv
     end
 end
