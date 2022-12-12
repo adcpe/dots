5d7fbb8dc47e6be7406ad311110419b1760d4bde
Andrés Del Carpio <adc@adc.pe>
2022-12-12 05:44:11 -0500

    Modify and move restore script

--- .config/dots/restore.fish
+++ .config/dots/restore.fish
@@ -4,16 +4,16 @@ read -l -P 'Do you want to continue? [y/N] ' confirm
 
 switch $confirm
     case Y y
-        set -l dir $HOME/.dots-git/ # set dots git directory
+        set -l dir $HOME/.config/dots/git/ # set dots git directory
         rm -R -f $dir
 
-        echo -e 'Cloning https://gitlab.com/adcpe/dots.git into .dots-git'
+        echo -e 'Cloning https://gitlab.com/adcpe/dots.git into $HOME/.config/dots/git/'
 
         git clone --bare https://gitlab.com/adcpe/dots.git $dir
 
-        echo -e 'Cloning https://gitlab.com/adcpe/dots.git into .dots-git'
         /usr/bin/git --git-dir=$dir --work-tree=$HOME checkout -f # checkout overwriting existing files
         /usr/bin/git --git-dir=$dir --work-tree=$HOME config status.showUntrackedFiles no # ignore untracked files
+        /usr/bin/git --git-dir=$dir --work-tree=$HOME config --local core.hooksPath $HOME/.config/git/hooks/ # set hooks dir
         /usr/bin/git --git-dir=$dir --work-tree=$HOME remote set-url origin git@gitlab.com:adcpe/dots.git # use ssh for remote
         echo -e 'Operation finished!'
     case '*'
