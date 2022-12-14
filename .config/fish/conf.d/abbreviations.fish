d0fe08d3dd1f07f6073c9232d8d8dabe813b3b0b
Andrés Del Carpio <adc@adc.pe>
2022-12-14 01:51:34 -0500

    Latest

--- .config/fish/conf.d/abbreviations.fish
+++ .config/fish/conf.d/abbreviations.fish
@@ -1,61 +1,89 @@
 # pacman/yay
-abbr -a -g paci 'yay -S'
-abbr -a -g pacun 'yay -Rcs'
-abbr -a -g pacu 'yay -Syu'
-abbr -a -g pacup 'yay -Syyu'
-abbr -a -g pacro 'yay -Rcs (yay -Qdtq)'
-abbr -a -g pacs 'yay -Ss'
-abbr -a -g pacsl 'yay -Qs'
-abbr -a -g paccc 'yay -Sc'
-abbr -a -g pacsi 'yay -Si'
+abbr paci 'yay -S'
+abbr pacun 'yay -Rcs'
+abbr pacu 'yay -Syu'
+abbr pacup 'yay -Syyu'
+abbr pacro 'yay -Rcs (yay -Qdtq)'
+abbr pacs 'yay -Ss'
+abbr pacsl 'yay -Qs'
+abbr paccc 'yay -Sc'
+abbr pacsi 'yay -Si'
+
+# git
+abbr g git
+abbr gs 'git s'
+abbr ga 'git add'
+abbr gaa 'git add --all'
+abbr gap 'git add --patch'
+abbr gc 'git commit -v -S'
+abbr gca 'git commit --amend -v -S'
+abbr gco 'git checkout'
+abbr gcob 'git checkout -b'
+abbr gp 'git push'
+abbr gpf 'git push --force-with-lease'
+abbr gpl 'git pull'
+abbr gcl 'git clone'
+abbr gf 'git fetch'
+abbr gfa 'git fetch --all'
+abbr gr 'git remote'
+abbr grb 'git rebase'
+abbr grbc 'git rebase --continue'
+abbr gb 'git branch'
+abbr gd 'git diff'
+abbr gl 'git ll'
 
 # dotfiles repo at $HOME
-abbr -a -g dadd 'dots add'
-abbr -a -g ddif 'dots diff'
-abbr -a -g dst 'dots status'
-abbr -a -g dpom 'dots push origin main'
-abbr -a -g dcom 'dots commit'
+abbr dadd 'dots add'
+abbr ddif 'dots diff'
+abbr dst 'dots status -sb'
+abbr dpom 'dots push origin main'
+abbr dcom 'dots commit'
+
+# kitty
+abbr kdif 'kitty +kitten diff'
+abbr kicat 'kitty +kitten icat'
+abbr kfonts 'kitty +list-fonts'
+abbr kssh 'kitty +kitten ssh'
 
 # pnpm
-abbr -a -g pn pnpm
-abbr -a -g pni 'pnpm i'
-abbr -a -g pnun 'pnpm un'
-abbr -a -g pnid 'pnpm i -D'
-abbr -a -g pnig 'pnpm i -g'
-abbr -a -g pnx 'pnpm -s dlx'
+abbr pn pnpm
+abbr pni 'pnpm i'
+abbr pnun 'pnpm un'
+abbr pnid 'pnpm i -D'
+abbr pnig 'pnpm i -g'
+abbr pnx 'pnpm -s dlx'
 
 # npm
-abbr -a -g npi 'npm i'
-abbr -a -g npun 'npm un'
-abbr -a -g npid 'npm i -D'
-abbr -a -g npig 'npm i -g'
+abbr npi 'npm i'
+abbr npun 'npm un'
+abbr npid 'npm i -D'
+abbr npig 'npm i -g'
 
 # protonvpn
-abbr -a -g protin 'protonvpn-cli login'
-abbr -a -g protc 'protonvpn-cli connect'
-abbr -a -g protcf 'protonvpn-cli connect -f'
-abbr -a -g protdc 'protonvpn-cli disconnect'
-abbr -a -g protst 'protonvpn-cli status'
+abbr protin 'protonvpn-cli login'
+abbr protc 'protonvpn-cli connect'
+abbr protcf 'protonvpn-cli connect -f'
+abbr protdc 'protonvpn-cli disconnect'
+abbr protst 'protonvpn-cli status'
 
 # asdf
-abbr -a -g afpad 'asdf plugin add'
-abbr -a -g afprm 'asdf plugin remove'
-abbr -a -g afpls 'asdf plugin list'
-abbr -a -g afpup 'asdf plugin update --all'
-abbr -a -g afi 'asdf install'
-abbr -a -g afun 'asdf uninstall'
-abbr -a -g afls 'asdf list'
-abbr -a -g aflsa 'asdf list all'
-abbr -a -g afcurr 'asdf current'
+abbr afpad 'asdf plugin add'
+abbr afprm 'asdf plugin remove'
+abbr afpls 'asdf plugin list'
+abbr afpup 'asdf plugin update --all'
+abbr afi 'asdf install'
+abbr afun 'asdf uninstall'
+abbr afls 'asdf list'
+abbr aflsa 'asdf list all'
+abbr afcurr 'asdf current'
 
 # exa/ls
-abbr -a -g ls 'exa --group-directories-first --icons'
-abbr -a -g lsa 'exa --group-directories-first --icons --all'
-abbr -a -g lsg 'exa --group-directories-first --icons --git'
-abbr -a -g lst 'exa --group-directories-first --icons --tree'
+abbr ls 'exa --group-directories-first --icons'
+abbr lsa 'exa --group-directories-first --icons --all'
+abbr lsg 'exa --group-directories-first --icons --git'
+abbr lst 'exa --group-directories-first --icons --tree'
 
 # various
-abbr -a -g cat bat
-abbr -a -g man batman
-abbr -a -g rm trash
-abbr -a -g ssh 'kitty +kitten ssh'
+abbr cat bat
+abbr man batman
+abbr rm trash
