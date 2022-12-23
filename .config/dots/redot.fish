ab170ea98cb22a65b936acce7b77c2a2730d8c66
Andrés Del Carpio <adc@adc.pe>
2022-12-23 01:15:31 -0500

    Fixes

--- .config/dots/redot.fish
+++ .config/dots/redot.fish
@@ -1,21 +1,21 @@
 #!/usr/bin/env fish
 echo -e 'This operation will overwrite the current settings in the $HOME directory.'
-read -l -P 'Do you want to continue? [y/N] ' confirm
+read -l -P 'Do you want to continue? [y/N] ' confirmation
 
-switch $confirm
+switch $confirmation
     case Y y
         set -l dir $HOME/.config/dots/git/ # set dots git directory
         rm -R -f $dir
 
-        echo -e 'Cloning https://gitlab.com/adcpe/dots.git into $HOME/.config/dots/git/'
+        echo -e 'Cloning https://github.com/adcpe/dots.git into ~/.config/dots/git/'
 
-        git clone --bare https://gitlab.com/adcpe/dots.git $dir
+        git clone --bare https://github.com/adcpe/dots.git $dir
 
-        /usr/bin/git --git-dir=$dir --work-tree=$HOME checkout -f # checkout overwriting existing files
-        /usr/bin/git --git-dir=$dir --work-tree=$HOME config status.showUntrackedFiles no # ignore untracked files
-        /usr/bin/git --git-dir=$dir --work-tree=$HOME config --local core.hooksPath $HOME/.config/dots/hooks/ # set hooks dir
-        /usr/bin/git --git-dir=$dir --work-tree=$HOME config --local core.hooksPath $HOME/.config/dots/ignore # set ignore file location
-        /usr/bin/git --git-dir=$dir --work-tree=$HOME remote set-url origin git@gitlab.com:adcpe/dots.git # use ssh for remote
+        git --git-dir=$dir --work-tree=$HOME checkout -f # checkout overwriting existing files
+        git --git-dir=$dir --work-tree=$HOME config status.showUntrackedFiles no # ignore untracked files
+        git --git-dir=$dir --work-tree=$HOME config --local core.hooksPath $HOME/.config/dots/hooks/ # set hooks dir
+        git --git-dir=$dir --work-tree=$HOME config --local core.excludesFile $HOME/.config/dots/ignore # set ignore file location
+        git --git-dir=$dir --work-tree=$HOME remote set-url origin git@github.com:adcpe/dots.git # use ssh for remote
         echo -e 'Operation finished!'
     case '*'
         echo -e 'Operation canceled.'
