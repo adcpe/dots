3aa2c5499ac462c9a49d072920e7ca67ba22d05a
Andrés Del Carpio <adc@adc.pe>
2023-01-04 22:44:37 -0500

    Update asdf vars

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -4,9 +4,12 @@ set -gx VISUAL kate
 set -gx EDITOR nano
 set -gx XDG_CONFIG_HOME $HOME/.config
 
-set -gx ASDF_NPM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/.default-npm-packages
-set -gx ASDF_GEM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/.default-gems
-set -gx ASDF_PYTHON_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/.default-python-packages
+set -gx ASDF_CONFIG_FILE $HOME/.config/asdf/asdfrc
+set -gx ASDF_DEFAULT_TOOL_VERSIONS_FILENAME $HOME/.config/asdf/tool-versions
+set -gx ASDF_DATA_DIR $HOME/.config/asdf
+set -gx ASDF_NPM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/default-npm-packages
+set -gx ASDF_GEM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/default-gems
+set -gx ASDF_PYTHON_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/default-python-packages
 
 set -gx GIT_AUTHOR_IDENT 'Andrés Del Carpio <adc@adc.pe>'
 set -gx GIT_COMMITTER_IDENT 'Andrés Del Carpio <adc@adc.pe>'
