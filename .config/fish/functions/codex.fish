e7e65d19fa373329f66a0d7c10802aed50c7dca9
Andrés Del Carpio <adc@adc.pe>
2023-01-05 01:10:42 -0500

    Update codex

--- .config/fish/functions/codex.fish
+++ .config/fish/functions/codex.fish
@@ -1,11 +1,11 @@
-function codex -a cmd -d 'Backups and installs VSCode extensions'
-    switch $cmd[1]
+function codex -d "Backups and installs VSCode extensions"
+    switch $argv[1]
         case install
-            echo 'Starting installation of extensions'
-            echo ''
+            echo "Starting installation of extensions"
+            echo ""
             while read -a line
                 eval code --install-extension $line
-                echo ''
+                echo ""
             end <$CODEX_FILE
             set -l num (count < $CODEX_FILE)
             echo "Finished installing $num extensions."
@@ -13,13 +13,13 @@ function codex -a cmd -d 'Backups and installs VSCode extensions'
             eval code --list-extensions >$CODEX_FILE
             set -l num (count < $CODEX_FILE)
             echo "Saved $num extensions."
-        case '' help
+        case "" help
             echo "Usage: codex install         Install extensions from list"
             echo "       codex backup          Backup extensions to list"
             echo "       codex help            Print this help message"
             echo "Variables:"
             echo "       \$CODEX_FILE  Extension list path. Location: $CODEX_FILE" | string replace --regex -- $HOME \~
-        case '*'
-            echo "codex: Unknown command: \"$cmd\"" >&2 && return 1
+        case "*"
+            echo "codex: Unknown command: \"$argv\"" >&2 && return 1
     end
 end
