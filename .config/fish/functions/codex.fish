e6d7e31c3ac00f011de0e77bb74650c1dd0ac1e6
Andrés Del Carpio <adc@adc.pe>
2023-01-05 02:14:02 -0500

    Enhance codex function
    - Added silent-backup and list options
    - Added completions

--- .config/fish/functions/codex.fish
+++ .config/fish/functions/codex.fish
@@ -13,9 +13,17 @@ function codex -d "Backups and installs VSCode extensions"
             eval code --list-extensions >$CODEX_FILE
             set -l num (count < $CODEX_FILE)
             echo "Saved $num extensions."
+        case silent-backup
+            eval code --list-extensions >$CODEX_FILE
+        case list
+            while read -a line
+                echo "$line"
+            end <$CODEX_FILE
         case "" help
             echo "Usage: codex install         Install extensions from list"
             echo "       codex backup          Backup extensions to list"
+            echo "       codex silent-backup   Backup extensions to list, no message"
+            echo "       codex list            List installed extensions"
             echo "       codex help            Print this help message"
             echo "Variables:"
             echo "       \$CODEX_FILE  Extension list path. Location: $CODEX_FILE" | string replace --regex -- $HOME \~
