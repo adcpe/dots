9a3e51a8db6fd9754e757f70d7525321cb7d2637
Andrés Del Carpio <adc@adc.pe>
2023-01-14 17:16:09 -0500

    Add conditionals for asdf path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
 fish_vi_key_bindings
-source /opt/asdf-vm/asdf.fish
+
+# check if asdf is installed and its path
+if test -e ~/.config/asdf/.asdf/asdf.fish
+    source ~/.config/asdf/.asdf/asdf.fish
+else if test -e ~/.asdf/asdf.fish
+    source ~/.config/asdf/.asdf/asdf.fish
+else if test -e /opt/asdf-vm/asdf.fish
+    source /opt/asdf-vm/asdf.fish
+end
+
 starship init fish | source
