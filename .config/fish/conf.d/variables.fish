8bcaa1482dea924bfb0d0c09b21e8cdb166c6b17
Andrés Del Carpio <adc@adc.pe>
2023-01-14 22:55:56 -0500

    Remove git variables from fish variables
    Can and was reading from file anyway

--- .config/fish/conf.d/variables.fish
+++ .config/fish/conf.d/variables.fish
@@ -11,11 +11,6 @@ set -gx ASDF_NPM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/default-npm-packages
 set -gx ASDF_GEM_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/default-gems
 set -gx ASDF_PYTHON_DEFAULT_PACKAGES_FILE $HOME/.config/asdf/default-python-packages
 
-set -gx GIT_AUTHOR_IDENT "Andrés Del Carpio <adc@adc.pe>"
-set -gx GIT_COMMITTER_IDENT "Andrés Del Carpio <adc@adc.pe>"
-set -gx GIT_DEFAULT_BRANCH main
-set -gx GIT_EDITOR nvim
-
 set -gx PNPM_HOME $HOME/.local/share/pnpm
 set -gx PATH $PNPM_HOME $PATH
 
