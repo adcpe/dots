46a7657f277259e75fac93d389092bea5aecb0ba
Andrés Del Carpio <adc@adc.pe>
2023-01-25 17:07:26 -0500

    Backup latest changes

--- .config/fish/functions/codex.fish
+++ .config/fish/functions/codex.fish
@@ -1,6 +1,6 @@
 function codex -d "Backups and installs VSCode extensions"
     switch $argv[1]
-        case install
+        case install in
             echo "Starting installation of extensions"
             echo ""
             while read -a line
@@ -9,24 +9,32 @@ function codex -d "Backups and installs VSCode extensions"
             end <$CODEX_FILE
             set -l num (count < $CODEX_FILE)
             echo "Finished installing $num extensions."
-        case backup
+        case backup b
             eval code --list-extensions >$CODEX_FILE
             set -l num (count < $CODEX_FILE)
             echo "Saved $num extensions."
-        case silent-backup
+        case silent-backup sb
             eval code --list-extensions >$CODEX_FILE
-        case list
+        case list ls
             while read -a line
                 echo "$line"
             end <$CODEX_FILE
-        case "" help
-            echo "Usage: codex install         Install extensions from list"
-            echo "       codex backup          Backup extensions to list"
-            echo "       codex silent-backup   Backup extensions to list, no message"
-            echo "       codex list            List installed extensions"
-            echo "       codex help            Print this help message"
-            echo "Variables:"
+        case "" help h
+            echo codex
+            echo "Script to mass-install extensions or create a list of currently installed extensions on VSCode."
+            echo ""
+            echo "Usage: codex [command]"
+            echo ""
+            echo Commands
+            echo "      install i           : Install extensions from list"
+            echo "      backup b            : Backup extensions to list"
+            echo "      silent-backup sb    : Backup extensions to list, no message"
+            echo "      list ls             : List installed extensions"
+            echo "      help h              : Print this help message"
+            echo ""
+            echo Variables
             echo "       \$CODEX_FILE  Extension list path. Location: $CODEX_FILE" | string replace --regex -- $HOME \~
+            echo ""
         case "*"
             echo "codex: Unknown command: \"$argv\"" >&2 && return 1
     end
--- .config/fish/functions/pk.fish
+++ .config/fish/functions/pk.fish
@@ -1,14 +1,19 @@
-function pk -d 'Search packages inside a browser.'
+function pk -d "Search packages inside a browser."
     switch $argv[1]
-        case main
+        case main m
             open "https://archlinux.org/packages/?sort=&q=$argv[2]"
-        case aur
+        case aur a
             open "https://aur.archlinux.org/packages?O=0&SeB=nd&K=$argv[2]&outdated=&SB=n&SO=a&PP=50"
-        case help ''
-            echo "Usage: pk main <package>          Search the main packages"
-            echo "       pk aur <package>           Search the AUR"
-            echo "       pk help                    Show this output"
-        case '*'
+        case help h ""
+            echo pk
+            echo "Small Fish script to search the Arch Linux repositories or the AUR."
+            echo ""
+            echo "Usage: pk [command] [query]"
+            echo Commands
+            echo "      main m [query]          Search the main packages"
+            echo "      aur a [query]           Search the AUR"
+            echo "      help h                  Show this output"
+        case "*"
             echo "pk: Unknown command: \"$argv\"" >&2 && return 1
     end
 end
